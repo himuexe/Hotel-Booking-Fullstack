6b1e9f88d82720fcdffb4286f7a574273e2e227e
Himanshu Sharma <sharmaslov@gmail.com>
2024-05-05 14:23:53 +0530

    test email

--- e2e-tests/tests/auth.spec.ts
+++ e2e-tests/tests/auth.spec.ts
@@ -9,9 +9,9 @@ test('should allow the user to sign-in', async ({ page }) => {
 
   await expect(page.getByRole("heading", { name: "Sign In" })).toBeVisible();
 
-  await page.locator("[name=email]").fill("1@1.com");
+  await page.locator("[name=email]").fill("sharmaslov@gmail.com");
 
-  await page.locator("[name=password]").fill("password123");
+  await page.locator("[name=password]").fill("passwordhai");
 
   await page.getByRole("button", { name: "Login" }).click();
 
